f2f11e7d8feeb2ea5c1d2e8a1b1560d0c0097c04
Jaryd Hibbert <jarydhibbert@gmail.com>
2024-09-24 15:24:12 -0600

    Week 2 created

---
+++ app/week-2/page.js
@@ -0,0 +1,9 @@
+import StudentInfo from "./student-info";
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo/>
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function StudentInfo() {
+    return(
+        <main>
+            <h1>jaryd Hibbert</h1>
+            <Link href="https://github.com/Hibbs18/cprg306-assignments.git">GitHub Repository</Link>
+        </main>
+    );
+}
